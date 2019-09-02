0b045bb5f4bdd0c4be42699ce14dc99c04db51bf
clvrbarros <cleversonferreirab@gmail.com>
2019-09-01 21:01:43 -0300

    test for album.js

---
+++ tests/album.spec.js
@@ -0,0 +1,72 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const { getAlbum, getAlbumTracks } = require('../src/album');
+
+const { expect } = chai;
+chai.use(sinonChai);
+global.fetch = require('node-fetch');
+
+
+describe('Album', () => {
+  let stubedFetch;
+
+  beforeEach(() => {
+    stubedFetch = sinon.stub(global, 'fetch');
+    stubedFetch.resolves({ json: () => ({ body: 'json' }) });
+  });
+
+  afterEach(() => {
+    stubedFetch.restore();
+  });
+
+  describe('smoke tests', () => {
+    it('should have getAlbum method', () => {
+      expect(getAlbum).to.exist;
+    });
+
+    it('should have getAlbumTracks method', () => {
+      expect(getAlbumTracks).to.exist;
+    });
+  });
+
+  describe('getAlbum', () => {
+    it('should call fecth method', () => {
+      const album = getAlbum();
+      expect(stubedFetch).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
+      expect(stubedFetch).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy');
+    });
+
+    it('should return the correct data', () => {
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
+      album.then((data) => {
+        expect(data).to.be.eql({ body: 'json' });
+      });
+    });
+  });
+
+  describe('getAlbumTracks', () => {
+    it('should call fetch method', () => {
+      const tracks = getAlbumTracks();
+      expect(stubedFetch).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const tracks = getAlbumTracks('4aawyAB9vmqN3uQ7FjRGTy');
+      expect(stubedFetch).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks');
+    });
+
+    it('shoud return the correct data', () => {
+      const tracks = getAlbumTracks('4aawyAB9vmqN3uQ7FjRGTy');
+      tracks.then((data) => {
+        expect(data).to.be.eql({ body: 'json' });
+      });
+    });
+  });
+});
