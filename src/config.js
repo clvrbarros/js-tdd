a2674059b3f49b21e6a1e314e94e92443bef595a
clvrbarros <cleversonferreirab@gmail.com>
2019-09-02 21:19:22 -0300

    add token

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
 const API_URL = 'https://api.spotify.com/v1';
+const TOKEN_API = 'BQDTOcBugcQ5QDV837nbp73yju5AY6AsESYg1SjclCVJUGbFG4DF9T9s8h2_u_T-RZrWeI7_s6RWt8l1wOPu_LoMP6to7Z1nCRbRVxcGXBeOWSxRUj7bTx2zNxL0qcwAU_-vcaSKHrjgjQQkqg';
 
-module.exports = API_URL;
+const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
+
+module.exports = { API_URL, HEADERS };
