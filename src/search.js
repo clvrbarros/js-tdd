e07a303f4634b82640265940b5a99c10a36bab19
clvrbarros <cleversonferreirab@gmail.com>
2019-09-02 21:19:38 -0300

    add authorization header

--- src/search.js
+++ src/search.js
@@ -1,23 +1,17 @@
-const API_URL = require('./config');
+const { API_URL, HEADERS } = require('./config');
 
-const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`)
+const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS)
   .then((data) => data.json());
 
 const searchArtists = (query) => {
   search(query, 'artist');
 };
 
-const searchAlbums = (query) => {
-  search(query, 'album');
-};
+const searchAlbums = (query) => search(query, 'album');
 
-const searchPlaylists = (query) => {
-  search(query, 'playlist');
-};
+const searchPlaylists = (query) => search(query, 'playlist');
 
-const searchTracks = (query) => {
-  search(query, 'track');
-};
+const searchTracks = (query) => search(query, 'track');
 
 module.exports = {
   search,
